ca8d2aef9d525faceee5234056e5e113c9c34fdd
Madhu-Yadav375 <myadav07012006@gmail.com>
2025-12-12 13:38:51 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,40 @@
+// console.log(2>1)
+// console.log(2==1);
+// console.log(2<1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("2" <= 1); //Compare only same datatype!
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+
+//we need to avoid this type of code
+
+//the reason is euality and comparison work differently
+
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+
+// === (strict check)
+console.log(2 === 2);
+console.log(2 == 2);
+
+console.log("2" == 2);
+console.log("2" === 2);//here, datatype bhi check hoga
+
+
+
+
+
+
+
+
+
+
+
+
+
