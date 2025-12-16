530b662a3e67d70fca3b1535e33da6f3a3ce9d95
Madhu-Yadav375 <myadav07012006@gmail.com>
2025-12-16 15:34:36 +0000

    JS execution

---
+++ .devcontainer/01_basics/03_basics/05_JSexecustion.js
@@ -0,0 +1,8 @@
+//JS execition context
+//1. global exe. context
+//2. f^n exe. context
+//3. eval exe. context
+
+//exe. happens in two phase
+//1.Memory creation phase/creation phase
+//2.Exe. phase
